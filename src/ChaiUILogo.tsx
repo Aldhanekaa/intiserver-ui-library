f3673c06ffc346c6cad1e34fe3481895711ed302
Aldhanekai <aldhanekadev@gmail.com>
2025-07-15 19:51:37 +0700

    Update ChaiUILogo.tsx

--- src/ChaiUILogo.tsx
+++ src/ChaiUILogo.tsx
@@ -3,16 +3,16 @@ export const Logo = () => {
     <a
       href="/"
       className="flex flex-none items-center rounded-xl font-semibold focus:opacity-80 focus:outline-none"
-      aria-label="Chai Builder">
+      aria-label="IntiServer UI">
       <img
         className="h-[30px] w-[30px] rounded-md lg:h-[30px] lg:w-[30px] xl:w-[30px]"
-        src="https://fldwljgzcktqnysdkxnn.supabase.co/storage/v1/object/public/chaibuilder-blob-storage/175ac8d8-37fe-4707-bb4a-3c0cd6a6db75/gVH7O-Ir_400x400.png"
+        src="https://intiserver.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fintiserver-logo.bb93b0d2.png&w=48&q=75"
         alt=""
         loading="lazy"
         height=""
         width=""
       />
-      <h2 className="ml-1 text-xl tracking-tight">Chai UI Blocks</h2>
+      <h2 className="ml-1 text-xl tracking-tight">IntiServer UI</h2>
     </a>
   );
 };
